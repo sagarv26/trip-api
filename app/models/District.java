44a6b60b728f932a2990e8529e2c0abe09135062
sagar <sagar@Sagars.local>
2019-08-25 18:31:27 +0530

    Updated

--- app/models/District.java
+++ app/models/District.java
@@ -5,12 +5,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import play.db.ebean.Model;
 
 @Entity
 @Table(name = "district")
 public class District extends Model {
+
 
 	private static final long serialVersionUID = 1L;
 
